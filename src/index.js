fcf9db8a97908d23de046cfd5b641176af0a6a7f
GuFvr <gustavo.vfavero@gmail.com>
2024-05-28 21:47:47 -0300

    feat: Implement race engine with random track blocks and dice rolls
    - Added `getRandomBlock` function to determine the type of track block (RETA, CURVA, CONFRONTO) randomly.
    - Implemented `playRaceEngine` function to simulate a 5-round race, displaying the type of block for each round and rolling dice for both players.
    - Initialized the race engine with `player1` and `player2`.

--- src/index.js
+++ src/index.js
@@ -18,8 +18,43 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1
 }
 
+async function getRandomBlock() {
+  let random = Math.random()
+  let result
+
+  switch (true) {
+    case random < 0.33:
+      result = 'RETA'
+      break
+    case random > 0.66:
+      result = 'CURVA'
+      break
+    default:
+      result = 'CONFRONTO'
+  }
+
+  return result
+}
+
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Rodada ${round}`)
+
+    let block = await getRandomBlock()
+    console.log(`Bloco: ${block}`)
+  }
+
+  let diceResult1 = await rollDice()
+  let diceResult2 = await rollDice()
+
+  let totalTestSkill1 = 0
+  let totalTestSkill2 = 0
+}
+
 ;(async function Main() {
   console.log(
     `🏁🚨 Corrida entre ${player1.NAME} e ${player2.NAME} começando...\n `
   )
+
+  await playRaceEngine(player1, player2)
 })()
